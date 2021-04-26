c9539b703d9ca666ae1be90169af09637a3be817
deepthi2007 <deepthik5656@gmail.com>
2021-04-26 12:43:42 +0530

    modal added

--- Screens/loginScreen.js
+++ Screens/loginScreen.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity,TextInput, Alert , Image } from 'react-native';
+import { View,
+   StyleSheet, 
+   Text, 
+   TouchableOpacity,
+   TextInput, 
+   Alert ,
+   Image , 
+   Modal , 
+  ScrollView ,
+KeyboardAvoidingView} from 'react-native';
 import db from '../config';
 import firebase from 'firebase';
 
@@ -8,21 +17,152 @@ export default class LoginScreen extends Component {
     super()
     this.state={
       emailId : '',
-      password: ''
+      password: '',
+      firstName:"",
+      lastName:"",
+      address:"",
+      contact:"",
+      confirmPassword:"",
+      isModalVisible:true
     }
   }
 
-  userSignUp = (emailId, password) =>{
-    firebase.auth().createUserWithEmailAndPassword(emailId, password)
-    .then((response)=>{
-      return Alert.alert("User Added Successfully")
-    })
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      return Alert.alert(errorMessage)
-    });
+  showModal = ()=>{
+    return(
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={this.state.isModalVisible}
+      >
+      <View style={styles.modalContainer}>
+        <ScrollView style={{width:'100%'}}>
+          <KeyboardAvoidingView style={styles.KeyboardAvoidingView}>
+          <Text
+            style={styles.modalTitle}
+            >Registration</Text>
+          <TextInput
+            style={styles.formTextInput}
+            placeholder ={"First Name"}
+            maxLength ={8}
+            onChangeText={(text)=>{
+              this.setState({
+                firstName: text
+              })
+            }}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder ={"Last Name"}
+            maxLength ={8}
+            onChangeText={(text)=>{
+              this.setState({
+                lastName: text
+              })
+            }}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder ={"Contact"}
+            maxLength ={10}
+            keyboardType={'numeric'}
+            onChangeText={(text)=>{
+              this.setState({
+                contact: text
+              })
+            }}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder ={"Address"}
+            multiline = {true}
+            onChangeText={(text)=>{
+              this.setState({
+                address: text
+              })
+            }}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder ={"Email"}
+            keyboardType ={'email-address'}
+            onChangeText={(text)=>{
+              this.setState({
+                emailId: text
+              })
+            }}
+          /><TextInput
+          
+            style={styles.formTextInput}
+            placeholder ={"Password"}
+            secureTextEntry = {true}
+            onChangeText={(text)=>{
+              this.setState({
+                password: text
+              })
+            }}
+          /><TextInput
+            style={styles.formTextInput}
+            placeholder ={"Confrim Password"}
+            secureTextEntry = {true}
+            onChangeText={(text)=>{
+              this.setState({
+                confirmPassword: text
+              })
+            }}
+          />
+          <View style={styles.modalBackButton}>
+            <TouchableOpacity
+              style={styles.registerButton}
+              onPress={()=>
+                this.userSignUp(this.state.emailId, this.state.password, this.state.confirmPassword)
+              }
+            >
+            <Text style={styles.registerButtonText}>Register</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.modalBackButton}>
+            <TouchableOpacity
+              style={styles.cancelButton}
+              onPress={()=>this.setState({"isModalVisible":false})}
+            >
+            <Text style={{color:'#ff5722'}}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+          </KeyboardAvoidingView>
+        </ScrollView>
+      </View>
+    </Modal>
+  )
+  }
+
+  userSignUp = (emailId, password,confirmPassword) =>{
+    if(password !== confirmPassword){
+        return Alert.alert("password doesn't match\nCheck your password.")
+    }else{
+      firebase.auth().createUserWithEmailAndPassword(emailId, password)
+      .then(()=>{
+        db.collection('users').add({
+          first_name:this.state.firstName,
+          last_name:this.state.lastName,
+          contact:this.state.contact,
+          email_id:this.state.emailId,
+          address:this.state.address
+        })
+        return  Alert.alert(
+             'User Added Successfully',
+             '',
+             [
+               {text: 'OK', onPress: () => this.setState({"isModalVisible" : false})},
+             ]
+         );
+      })
+      .catch((error)=> {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        return Alert.alert(errorMessage)
+      });
+    }
   }
 
   userLogin = (email,password)=>{
@@ -48,6 +188,8 @@ export default class LoginScreen extends Component {
     return(
       <View style={styles.container}>
         <View style={styles.profileContainer}>
+
+          <View>{this.showModal()}</View>
           
         <Image
           style={styles.tinyLogo}
@@ -87,7 +229,7 @@ export default class LoginScreen extends Component {
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.button}
-            onPress={()=>{this.userSignUp(this.state.emailId, this.state.password)}}
+            onPress={()=>{this.setState({isModalVisible:true})}}
             >
             <Text style={styles.buttonText}>SignUp</Text>
           </TouchableOpacity>
@@ -154,5 +296,59 @@ const styles = StyleSheet.create({
   tinyLogo: {
     width: 200,
     height: 200,
+  },
+  KeyboardAvoidingView:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  modalTitle :{
+    justifyContent:'center',
+    alignSelf:'center',
+    fontSize:30,
+    color:'#ff5722',
+    margin:50
+  },
+  modalContainer:{
+    flex:1,
+    borderRadius:20,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"#ffff",
+    marginRight:30,
+    marginLeft : 30,
+    marginTop:80,
+    marginBottom:80,
+  },
+  formTextInput:{
+    width:"75%",
+    height:35,
+    alignSelf:'center',
+    borderColor:'#ffab91',
+    borderRadius:10,
+    borderWidth:1,
+    marginTop:20,
+    padding:10
+  },
+  registerButton:{
+    width:200,
+    height:40,
+    alignItems:'center',
+    justifyContent:'center',
+    borderWidth:1,
+    borderRadius:10,
+    marginTop:30
+  },
+  registerButtonText:{
+    color:'#ff5722',
+    fontSize:15,
+    fontWeight:'bold'
+  },
+  cancelButton:{
+    width:200,
+    height:30,
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:5,
   },
 })
